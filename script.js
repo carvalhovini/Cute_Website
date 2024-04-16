6e82129dee813ceb2f2709be379f09dd5fd0419b
Vinicius Carvalho Silva <68484993+carvalhovini@users.noreply.github.com>
2024-04-16 17:40:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Contagem regressiva do tempo juntos
-    var startDate = new Date("2022-10-18"); // Coloque a data que começaram a namorar aqui
+    var startDate = new Date("2022-09-18"); // Coloque a data que começaram a namorar aqui
     var countdownElement = document.getElementById("countdown");
 
     function updateCountdown() {
         var currentDate = new Date();
         var timeDifference = currentDate.getTime() - startDate.getTime();
+    
         var daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        countdownElement.textContent = daysDifference + " dias juntos!";
+        var hoursDifference = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutesDifference = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var secondsDifference = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        
+        countdownElement.textContent = daysDifference + " dias, " + hoursDifference + " horas, " + minutesDifference + " minutos e " + secondsDifference + " segundos juntos!";
     }
+    
 
     updateCountdown();
     setInterval(updateCountdown, 1000);
@@ -17,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var skyImageElement = document.getElementById("sky-image");
     var nasaApiKey = "Y9iyTNJXcHTTL57E2MzSZEXQLICcOyyogjhs6pcr";
 
-    fetch("https://api.nasa.gov/planetary/earth/assets?lon=0&lat=0&date=2022-10-18&dim=0.1&api_key=" + nasaApiKey)
+    fetch("https://api.nasa.gov/planetary/earth/assets?lon=0&lat=0&date=2022-09-18&dim=0.1&api_key=" + nasaApiKey)
         .then(response => response.json())
         .then(data => {
             skyImageElement.src = data.url;
